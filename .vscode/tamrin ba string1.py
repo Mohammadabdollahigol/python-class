5580b938da37b49a3bc9b6bcddee78ad1f4e6b76
Mohammad Abdolahi Gol <parsaag1358@gmail.com>
2023-05-05 18:08:10 +0330

    version02

---
+++ .vscode/tamrin ba string1.py
@@ -0,0 +1,13 @@
+s='abc'
+print(s)
+print ("tedad horoof moteghayer(len)=", len(s)) 
+b=s[0]*3+s[1]*3+s[2]*3
+print("s[0]*3+s[1]*3+s[2]*3=",b)
+ss=s[0]*3+s[1]*3+s[2]*3
+print("s[0]*3+s[1]*3+s[2]*3=" ,ss)
+sss=ss[:0]+'xxx'+ss[3:]
+print("ss[:0]+'xxx'+ss[3:]=", sss)
+f=sss.upper()
+print("tabdil be horoof bozorg(.uper)=", f)
+g=f.lower()
+print("tabdil be horoof koochak(.uper)=", g)
---
+++ .vscode/tamrin ba string2.py
@@ -0,0 +1,7 @@
+s='aaabbbccc'
+s=s[:0]+'xxx'+s[3:]
+print(s)
+f=s.upper()
+print(f)
+g=f.lower()
+print(g)
